016bd4ebe897fc73d2faf5dca46e431fc19afb53
TanyaMixajlova <tanya26reg2013@yandex.ru>
2026-08-16 13:03:49 +0300

    исправила getName() и компоратор

--- src/org/skypro/skyshop/product/SearchableComparator.java
+++ src/org/skypro/skyshop/product/SearchableComparator.java
@@ -5,9 +5,9 @@ import java.util.Comparator;
 public class SearchableComparator implements Comparator<Searchable> {
     @Override
     public int compare(Searchable o1, Searchable o2) {
-        int integerCompare = Integer.compare(o1.getName().length(), o2.getName().length());
+        int integerCompare = Integer.compare(o2.getName().length(), o1.getName().length());
         if (integerCompare == 0) {
-            return o2.getName().compareTo(o1.getName());
+            return o1.getName().compareTo(o2.getName());
         }
         return integerCompare;
     }
